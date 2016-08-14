dc5eb83525648c3f62306aceaf99e622e22124a9
Vinay Kumar <vinay.27071994@gmail.com>
2016-08-14 20:09:53 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+// initializing the animaton
+new WOW().init();
+
+
+// for smooth scrolling of the page
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
